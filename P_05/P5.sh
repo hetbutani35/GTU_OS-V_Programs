a87a4c66ecc07582a7f3fbc4f1c5835374a42578
hetbutani35 <56062114+hetbutani35@users.noreply.github.com>
2021-04-27 12:22:03 +0530

    Create P5.sh

---
+++ P_05/P5.sh
@@ -0,0 +1,20 @@
+#shell script to print prime numbers
+
+echo "Enter the number upto which you want Prime numbers : "
+read n
+
+for ((i=1;i<=n;i++))
+do
+	flag=0
+	for((j=2;j<i;j++))
+	do
+		if [ `expr $i % $j` -eq 0 ]
+		then
+			flag=1
+		fi
+	done
+	if [ $flag -eq 0 ]
+	then 
+		echo $i
+	fi
+done
